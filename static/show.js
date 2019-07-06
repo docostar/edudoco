3582a07339460b427f4de51edfc0332384812ad1
test-git <dholariyarahul@gmail.com>
2019-07-06 18:12:09 +0530

    Answer background changed

--- static/show.js
+++
@@ -1,8 +0,0 @@
-// Function to change heading to say goodbye
-function right()
-{
-    alert("right_option")
-    //document.querySelector('#A').innerHTML = 'Goodbye!';
-    document.querySelector('#A').innerHTML = "Goodbye!";
-    //document.querySelector('h1').innerHTML = 'Goodbye!';
-}
---
+++ static/show1.js
@@ -0,0 +1,5 @@
+function right(que,right_option)
+{
+    var thediv = document.getElementById(que+right_option);
+    thediv.classList.add("greenbag");  //style.color='#66ff33'
+}
