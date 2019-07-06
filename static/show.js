4f520947077f961bac5083c2dbac3d14c7ca43a9
test-git <dholariyarahul@gmail.com>
2019-07-06 11:50:24 +0530

    Adding right option colur

---
+++ static/show.js
@@ -0,0 +1,8 @@
+// Function to change heading to say goodbye
+function right()
+{
+    alert("right_option")
+    //document.querySelector('#A').innerHTML = 'Goodbye!';
+    document.querySelector('#A').innerHTML = "Goodbye!";
+    //document.querySelector('h1').innerHTML = 'Goodbye!';
+}
